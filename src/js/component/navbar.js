c8a6cd17013218976fa17dac361439988df4afb7
Joanzam <joanzam.com@gmail.com>
2024-11-09 08:14:34 +0000

    Mi commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,12 +5,11 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Mi App de Contactos</span>
 			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+			<div className="ml-auto d-flex gap-3">
+				<Link to="/contacts" className="btn btn-primary shadow-lg">Go to Contacts</Link>
+				<Link to="/add-contact" className="btn btn-success shadow-lg">Add New Contact</Link>
 			</div>
 		</nav>
 	);
